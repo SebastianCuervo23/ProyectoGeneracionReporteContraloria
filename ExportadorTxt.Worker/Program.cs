90931e6f841c72f5afac5a75b0bbdbe94dcfeb2f
Brayan Sebastian Cuervo Lancheros <brayan.cuervol@ssf.gov.co>
2026-04-10 15:04:07 -0500

    Automatizacion consultas y generacion completa de archivos

--- ExportadorTxt.Worker/Program.cs
+++ ExportadorTxt.Worker/Program.cs
@@ -1,34 +1,3 @@
-/*using ExportadorTxt.Application.Interfaces;
-using ExportadorTxt.Application.UseCases;
-using ExportadorTxt.Infrastructure.Infraestructura;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using ExportadorTxt.Worker;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddSingleton<ISubsidioEspecieRepositorio, SubsidioEspecieRepositorio>();
-builder.Services.AddSingleton<IArchivoService, ArchivoService>();
-builder.Services.AddSingleton<GenerarArchivoSubsidioUseCase>();
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-///using var scope = host.Services.CreateScope();
-
-var useCase = scope.ServiceProvider.GetRequiredService<GenerarArchivoSubsidioUseCase>();
-
-int anioMes = 202401;
-
-await useCase.EjecutarAsync(anioMes);
-
-Console.WriteLine("Proceso finalizado.");
-
-Console.ReadLine();
-
-///
-host.Run();
-*/
-
 using ExportadorTxt.Application.Handlers;
 using ExportadorTxt.Application.Interfaces;
 using ExportadorTxt.Domain.Entidades;
